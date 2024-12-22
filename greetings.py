22d076bfc9ac0391071839191ca4d724f8cff552
dschinghis2008 <96759968+dschinghis2008@users.noreply.github.com>
2024-12-22 12:15:26 +0700

    note manager step 1

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = 'd2024'
+title = 'note manager'
+content = 'python project'
+status = 'processed'
+created_date = '22.12.2024'
+issue_date = '31.12.2024'
+
+print('Описание переменной: ', username)
+print('Заголовок: ', title)
+print('Описание: ', content)
+print('Статус: ', status)
+print('Дата создания: ', created_date)
+print('Дата истечения: ', issue_date)
