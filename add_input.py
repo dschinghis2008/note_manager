aa3213464dc36a2e739f654f64d54ed568eacf4e
dschinghis2008 <96759968+dschinghis2008@users.noreply.github.com>
2024-12-24 12:58:07 +0700

    note manager step 3

---
+++ add_input.py
@@ -0,0 +1,13 @@
+username = input("введите имя пользователя: ")
+title = input("введите заголовок: ")
+content = input("введите описание: ")
+status = input("введите статус: ")
+created_date = input('введите дату создания заметки в формате "dd.mm.yyyy": ')
+issue_date = input('введите дату истечения заметки в формате "dd.mm.yyyy": ')
+
+print('Имя пользователя: ', username)
+print('Заголовок: ', title)
+print('Описание: ', content)
+print('Статус: ', status)
+print('Дата создания: ', created_date)
+print('Дата истечения: ', issue_date)
