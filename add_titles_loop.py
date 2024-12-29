651990dd40e2fa080d289ac0b8b64eaf4b121655
dschinghis2008 <96759968+dschinghis2008@users.noreply.github.com>
2024-12-29 17:00:21 +0700

    note manager step 8

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -2,11 +2,11 @@ content = ''
 set_titles = {content}
 
 while content != 'stop':
-    content = input('Введите заголовок или "stop" для завершения ввода: ')
-    set_titles.add(content)
+    content = input('Введите заголовок или "stop" для завершения ввода:')
+    if content != 'stop':
+        set_titles.add(content)
 else:
     print('Ввод заголовков завершен. Дубли исключены. Введены следующие заголовки:')
-set_titles.remove('stop')
 set_titles.remove('')
 for title in set_titles:
     print(title)
--- check_deadline.py
+++ check_deadline.py
@@ -1,7 +1,31 @@
 import datetime
 
-now = datetime.datetime.now()
-issue_date = datetime.datetime(2025, 1, 31)
+
+def get_now():
+    return datetime.datetime.now()
+
+
+def get_issue_date():
+    dt_format = ''
+    f = input('Выберите формат даты:\n1 "dd.mm.yy"\n2 "yyyy-mm-dd"')
+    if f == '1':
+        dt_format = '%d.%m.%y'
+    elif f == '2':
+        dt_format = '%Y-%m-%d'
+    else:
+        dt_format = '%d.%m.%Y'
+
+    str_dt = input('Введите дату истечения заметки в формате "' + dt_format + '":')
+    return datetime.datetime.strptime(str_dt, dt_format)
+
+
+now = get_now()
+issue_date = get_issue_date()
 print('Сейчас: ', now.strftime('%d.%m.%y'))
-print('Дата действия заметки: ', issue_date.strftime('%d.%m.%y'))
-print('Дата действия заметки истекла? ', now > issue_date)
+print('Дата действия заметки: ', issue_date.strftime('%d.%m.%Y'))
+check_deadline = now > issue_date
+print('Дата действия заметки истекла? ', check_deadline)
+if check_deadline:
+    print('Дата заметки истекла ', now - issue_date, ' назад')
+else:
+    print('До истечения даты: ', issue_date - now)
